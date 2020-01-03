4991298f823c28a8241954e577b36a7810c442a8
Lucas Lamar <lucass.lamar@gmail.com>
2020-01-03 18:04:06 -0300

    estila layout de register && login

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <input type="text" placeholder="Seu Nome" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho Login</Link>
+      </form>
+    </>
+  );
 }
