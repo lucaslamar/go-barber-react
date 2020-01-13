a7f3f91c7118ec672f0f6a21ef25c64d23ed596c
Lucas Lamar <lucass.lamar@gmail.com>
2020-01-13 19:31:07 -0300

    [fix]configura redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,6 +1,6 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
-import { reactotronSaga } from 'reactotron-redux-saga';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
@@ -8,6 +8,5 @@ if (process.env.NODE_ENV === 'development') {
     .use(reactotronSaga())
     .connect();
   tron.clear();
-
   console.tron = tron;
 }
