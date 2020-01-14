0ba326eb87cb17b77e170990ca33e4ebd4abc17f
Lucas Lamar <lucass.lamar@gmail.com>
2020-01-14 13:02:01 -0300

    armazena perfil autenticado mesmo apos reload

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persisteReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -13,8 +16,9 @@ const sagaMiddleware = createSagaMiddware(sagaMonitor);
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,5 +1,3 @@
 import { all } from 'redux-saga/effects';
 
-export default function* rootSaga() {
-  yield all([]);
-}
+export default all([]);
