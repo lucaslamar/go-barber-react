d1ea293560443ebe7e430d68a3ca91614e839d00
Lucas Lamar <lucass.lamar@gmail.com>
2020-03-26 15:22:59 -0300

    estiliza header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-roxa.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Gobarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Lucas Lamar</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Lamar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
