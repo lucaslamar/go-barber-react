a5c53de1801ab4b39f78a5b6b797f3cfb39147b2
Lucas Lamar <lucass.lamar@gmail.com>
2020-03-30 14:45:11 -0300

    cria pagina perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="email@servidor.com" />
+
+        <hr />
+
+        <Input name="password" type="oldPassword" placeholder="Senha Atual" />
+        <Input name="password" type="password" placeholder="Nova Senha" />
+        <Input
+          name="ConfirmPassword"
+          type="Password"
+          placeholder="Confirmação de Senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair </button>
+    </Container>
+  );
 }
