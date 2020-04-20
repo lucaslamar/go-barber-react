903400eb528e87437f106a735fca1c45ca62626b
Lucas Lamar <lucass.lamar@gmail.com>
2020-04-20 11:27:01 -0300

    [FEAT] - Estiliza dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Lucas Lamar</span>
+        </Time>
+
+        <Time available>
+          <strong>08:00</strong>
+          <span>Lucas Lamar</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
