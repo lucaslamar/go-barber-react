403cadca6d8fb107a79922155e7f6bac10d9d719
Lucas Lamar <lucass.lamar@gmail.com>
2020-04-21 18:00:20 -0300

    [fix dashboard]

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useMemo, useEffect } from 'react';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import {
   format,
   subDays,
@@ -10,13 +9,17 @@ import {
   isBefore,
   isEqual,
   parseISO,
+  setMilliseconds,
 } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
-import pt from 'date-fns/locale/pt-BR';
-import { Container, Time } from './styles';
+import pt from 'date-fns/locale/pt';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-const range = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
 export default function Dashboard() {
   const [schedule, setSchedule] = useState([]);
@@ -27,8 +30,12 @@ export default function Dashboard() {
     [date]
   );
 
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
   useEffect(() => {
-    async function loadSchedule() {
+    async function loadScedule() {
       const response = await api.get('schedule', {
         params: { date },
       });
@@ -36,13 +43,16 @@ export default function Dashboard() {
       const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
       const data = range.map(hour => {
-        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
         const compareDate = utcToZonedTime(checkDate, timezone);
 
         return {
           time: `${hour}:00h`,
           past: isBefore(compareDate, new Date()),
-          appointment: response.data.find(a =>
+          appointment: response.data.appointment.find(a =>
             isEqual(parseISO(a.date), compareDate)
           ),
         };
@@ -50,13 +60,8 @@ export default function Dashboard() {
 
       setSchedule(data);
     }
-
-    loadSchedule();
+    loadScedule();
   }, [date]);
-
-  function handlePrevDay() {
-    setDate(subDays(date, 1));
-  }
 
   function handleNextDay() {
     setDate(addDays(date, 1));
