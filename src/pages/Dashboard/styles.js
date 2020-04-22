8dc962db64dc9aee81af9df3cf8c1f8a33ec263f
Lucas Lamar <lucass.lamar@gmail.com>
2020-04-22 12:21:28 -0300

    error partes brancas

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
+  min-height: 100%;
 
   display: flex;
   flex-direction: column;
